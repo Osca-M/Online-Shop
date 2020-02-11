bd5cf32bfbc1dcaec872a650f1e284a8c574664f
Osca-M <oscamwongera@gmail.com>
2020-02-11 11:51:50 +0300

    Project skeleton

---
+++ README.md
@@ -0,0 +1,2 @@
+# Online-Shop
+drf backend for an e-commerce website
---
+++ core/settings/production.py
@@ -0,0 +1,19 @@
+from .base import *
+DEBUG = False
+
+from decouple import config
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': config('PROD_DB_NAME'),
+        'USER': config('PROD_DB_USER'),
+        'PASSWORD': config('PROD_DB_PASSWORD'),
+        'HOST': config('PROD_DB_HOST'),
+        'PORT': config('PROD_DB_PORT')
+    }
+}
