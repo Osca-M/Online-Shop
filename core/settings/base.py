98d0a92d16361033a8df9c7fff6ff0de7a82d5a0
Osca-M <oscamwongera@gmail.com>
2021-03-04 14:54:11 +0300

    remove login url

--- core/settings/base.py
+++ core/settings/base.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    
+
     'oauth2_provider',
     'rest_framework',
     'corsheaders',
