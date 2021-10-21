86450d36d1b8ea6909f4fb6d0f1babb0fe794646
Osca Mwongera <oscamwongera@gmail.com>
2021-10-21 16:23:23 +0300

    fix debug mode via environment variable

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', True)
 
 from decouple import config
 
@@ -22,39 +22,3 @@ DATABASES = {
         'PORT': config('PROD_DB_PORT')
     }
 }
-
-# from .base import *
-#
-# DEBUG = True
-#
-# ALLOWED_HOSTS = ['*']
-#
-# from .base import *
-#
-# DEBUG = False
-#
-# ALLOWED_HOSTS = ['192.168.214.91']
-#
-# DATABASES = {
-#         'default': {
-#                 'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#                 'NAME': 'backend_test',
-#                 'USER': 'situma',
-#                 'PASSWORD': 'mercury',
-#                 'OPTIONS': {
-#                     'options': '-c search_path=public'
-#                 },
-#                 'HOST': '192.168.214.137',
-#                 'PORT': '5432',
-#                 'TEST': {
-#                         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#                         'NAME': 'backend_test',
-#                         'USER': 'situma',
-#                         'PASSWORD': 'mercury',
-#                         'OPTIONS': {
-#                             'options': '-c search_path=situma_test'
-#                         },
-#                 },
-#         }
-# }
-
